b41ed3b86e76594c9e5bf05df26dba7089f02c12
SreypichTO <sreypichdarani2003@gmail.com>
2023-11-17 07:04:53 +0700

    first commit -b tp2

---
+++ src/components/header.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="header">
+        <div class="header-text">{{ text }}</div>
+
+        <div class="wrap1">
+            <div style="font-family: Montserrat;font-size: 16px;font-weight: 700;line-height: 24px;letter-spacing: 0em;text-align: left;"
+            >All</div>
+            <div>Milks & Diaries</div>
+            <div>Coffee & Tea</div>
+            <div>Pet Foods</div>
+            <div>Meats</div>
+            <div>Vegetables</div>
+            <div>Fruits</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "headRow",
+        props:{
+            text:String,
+            
+        }
+    }
+</script>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap');
+
+@import url("https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Lato:ital,wght@0,300;0,400;1,300&family=Quicksand:wght@300;400;700&display=swap");
+.header{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items:end;
+    padding: 10px;
+    padding-bottom: 60px ;
+}
+.wrap1{
+    display: flex;
+    justify-content: space-around;
+    /* background-color: aqua; */
+    width: 45%;
+    font-size: 16px;
+    font-weight: 400;
+    font-family: Lato;
+}
+.header-text{
+    font-family: Quicksand;
+    font-size: 32px;
+    font-weight: 700;
+    /* line-height: 40px; */
+    letter-spacing: -0.0004em;
+    text-align: left;
+
+}
+</style>
---
+++ src/components/productCard.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="pro-card">
+        <div class="productLabel" :style="{ backgroundColor:LabelBg}">
+            <div class="textLabel" >{{ text }}</div>
+        </div>
+        <img src="../assets/image/mango.svg" alt="Mango">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "productCard",
+        props: {
+            text: String,
+            LabelBg: String,
+        },
+  };
+    
+</script>
+
+<style scoped>
+.pro-card{
+    width: 298px;
+    height: 402px;
+    border-radius: 10px;
+    border: 1px solid rgba(188, 227, 201, 1);
+    background-color: white;
+
+}
+img{
+    width: 201.89px;
+    height: 144.93px;
+    position: relative;
+    top: 61.81px;
+    left: 37.11px;
+}
+.productLabel{
+    width:  58px;
+    height: 32px;
+    position: relative;
+    top: 20px;
+    /* background-color: rgba(59, 183, 126, 1); */
+    padding: 10px, 15px, 10px, 15px;
+    border-top-right-radius: 30px;
+    border-bottom-right-radius: 30px;
+    
+    
+}
+.textLabel{
+    text-align: center;
+    font-family: Lato;
+    color: rgba(255, 255, 255, 1);
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 12px;
+    letter-spacing: 0em;
+    position: relative;
+    top: 10px;
+    
+    
+
+}
+
+</style>
